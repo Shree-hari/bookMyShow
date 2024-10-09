7057b7b7eaf7df9e34760f27cfc67a871844765c
Harsh <31119605+Shree-hari@users.noreply.github.com>
2024-10-10 03:57:45 +0530

    updated cors

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config(); // for .env file
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -10,8 +11,24 @@ const showRouter = require('./routes/showRoute');
 const bookingRouter = require('./routes/bookingRoute');
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: "*",
+        allowedHeaders: ["Content-Type", "Authorization"],
+        method: ["GET", "POST", "PUT", "DELETE"],
+    }
+));
+
+const clientBuildPath = path.join(__dirname, "../client/build");
+console.log(clientBuildPath);
+
+app.use(express.static(clientBuildPath));
+
+// app.use(cors())
 app.use(express.json());
+
+
+app.use("/api/bookings/verify", express.raw({ type: "application/json" }));
 
 connectDB();
 
@@ -21,6 +38,14 @@ app.use('/api/movies', movieRouter);
 app.use('/api/theatres', theatreRouter);
 app.use("/api/shows", showRouter);
 app.use('/api/bookings', bookingRouter);
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(clientBuildPath, "index.html"));
+});
+
+app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+});
 
 app.listen(8082, () => {
     console.log('Server is running on port 8082');
